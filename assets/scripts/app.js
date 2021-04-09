62c475773b71a060e4ef4132729b35ca47c209c4
SafrullahIsmail <0821smska@gmail.com>
2021-04-09 10:12:08 +0700

    basic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,68 @@
-alert('This works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEnteries = [];
+
+function getUserInput(){
+    return parseInt(userInput.value);
+}
+
+function setOutput(operator, initialResult, calcNumber){
+    const calcDesc = `${initialResult} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDesc);
+}
+
+function setToObject(operation, prevResult, number, result){
+    const logEntry = {
+        operation: operation,
+        prevResult: prevResult,
+        number: number,
+        result: result
+    };
+    logEnteries.push(logEntry);
+    console.log(logEnteries);
+}
+
+function calculation(calcType){
+    const enteredNum = getUserInput();
+    const initialResult = currentResult;
+    let mathOperator;
+    if(calcType == 'ADD'){
+        currentResult += enteredNum;
+        mathOperator = '+';
+    }
+    else if(calcType == 'SUBTRACT'){
+        currentResult -= enteredNum;
+        mathOperator = '-';
+    }
+    else if(calcType == 'MULTIPLY'){
+        currentResult *= enteredNum;
+        mathOperator = '-';
+    }
+    else if(calcType == 'DIVIDE'){
+        currentResult /= enteredNum;
+        mathOperator = '-';
+    }
+    setOutput(mathOperator, initialResult, enteredNum);
+    setToObject('ADD', initialResult, enteredNum, currentResult);
+}
+
+function add(){
+    calculation('ADD');
+}
+
+function subtract(){
+    calculation('SUBTRACT');
+}
+
+function multiply(){
+    calculation('MULTIPLY');
+}
+
+function divide(){
+    calculation('DIVIDE');
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
